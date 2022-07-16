12d81f05e263441670471fbfb58c2d13e64f0a46
Leyder Quintero <lele.quinter@gmail.com>
2022-07-16 17:38:17 -0500

    Creamos modulos de productos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,9 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/components/header/header.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material.module';
-import { DominicodeComponent } from './pages/dominicode/dominicode.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, DominicodeComponent],
+  declarations: [AppComponent, HeaderComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
