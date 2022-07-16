e85841e594264a07a9eaac2d556458b2519b0591
Leyder Quintero <lele.quinter@gmail.com>
2022-07-16 16:41:55 -0500

    rutas y componentes para rutas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,10 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/components/header/header.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material.module';
+import { DominicodeComponent } from './pages/dominicode/dominicode.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent],
+  declarations: [AppComponent, HeaderComponent, DominicodeComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
