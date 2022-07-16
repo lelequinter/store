86c04fee8fcfddc2fb6a15a397c2dd9d9a47f569
Leyder Quintero <lele.quinter@gmail.com>
2022-07-16 11:45:31 -0500

    instalacion de angular Material, creacion de componente inline angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/components/header/header.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material.module';
 
 @NgModule({
   declarations: [AppComponent, HeaderComponent],
-  imports: [BrowserModule, AppRoutingModule, FormsModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
   exports: [],
