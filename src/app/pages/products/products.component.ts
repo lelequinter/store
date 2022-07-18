1e5fc908de8315ae647a85653e020a5f10c98f16
Leyder Quintero <lele.quinter@gmail.com>
2022-07-18 11:15:49 -0500

    Mostramos la respuesta json en el componente Products

--- src/app/pages/products/products.component.ts
+++ src/app/pages/products/products.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { tap } from 'rxjs';
+import { Product } from './interface/product.interface';
 import { ProductsService } from './services/products.service';
 
 @Component({
@@ -8,12 +9,14 @@ import { ProductsService } from './services/products.service';
   styleUrls: ['./products.component.scss'],
 })
 export class ProductsComponent implements OnInit {
+  products!: Product[];
+
   constructor(private productSvc: ProductsService) {}
 
   ngOnInit(): void {
     this.productSvc
       .getProducts()
-      .pipe(tap((res) => console.log(res)))
+      .pipe(tap((products: Product[]) => (this.products = products)))
       .subscribe();
   }
 }
