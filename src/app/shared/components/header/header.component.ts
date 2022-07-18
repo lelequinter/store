5509cee1a434e22a10b882e7ccd5478f35c1f681
Leyder Quintero <lele.quinter@gmail.com>
2022-07-18 17:53:37 -0500

    Utilizamos el ShoppingCart Service

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,12 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { ShoppingCartService } from '../../services/shopping-cart.service';
 
 @Component({
   selector: 'app-header',
-  template: `
-  <mat-toolbar color="primary">
+  template: ` <mat-toolbar color="primary">
     <span>My Store</span>
+    {{ quantity$ | async | json }} - Quantity {{ total$ | async | json }} -
+    Total
   </mat-toolbar>`,
   styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent{
+export class HeaderComponent {
+  quantity$ = this.shoppingCartSvc.quantityAction$;
+  total$ = this.shoppingCartSvc.totalAction$;
+  cart$ = this.shoppingCartSvc.cartAction$;
+
+  constructor(private shoppingCartSvc: ShoppingCartService) {}
 }
