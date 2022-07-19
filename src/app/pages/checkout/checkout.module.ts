d2941a89b5c4b360913b2f64976edf8b02c8a380
Leyder Quintero <lele.quinter@gmail.com>
2022-07-19 11:36:16 -0500

    utilizamos obervables de shoppingCartService y Renderizamos

--- src/app/pages/checkout/checkout.module.ts
+++ src/app/pages/checkout/checkout.module.ts
@@ -5,9 +5,10 @@ import { CheckoutRoutingModule } from './checkout-routing.module';
 import { CheckoutComponent } from './checkout.component';
 import { MaterialModule } from 'src/app/material.module';
 import { FormsModule } from '@angular/forms';
+import { DetailsComponent } from './details/details.component';
 
 @NgModule({
-  declarations: [CheckoutComponent],
+  declarations: [CheckoutComponent, DetailsComponent],
   imports: [CommonModule, CheckoutRoutingModule, FormsModule, MaterialModule],
 })
 export class CheckoutModule {}
