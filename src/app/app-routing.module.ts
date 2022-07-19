2bd7708c57650e8a6adf113c7f01e6e84f5c086c
Leyder Quintero <lele.quinter@gmail.com>
2022-07-19 11:02:46 -0500

    Redirect a la products page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,7 +8,9 @@ const routes: Routes = [
       import('./pages/products/products.module').then((m) => m.ProductsModule),
   },
   { path: 'checkout', loadChildren: () => import('./pages/checkout/checkout.module').then(m => m.CheckoutModule) },
+  { path: '', redirectTo: '/products', pathMatch: 'full' },
   { path: '**', redirectTo: '', pathMatch: 'full' },
+
 ];
 
 @NgModule({
